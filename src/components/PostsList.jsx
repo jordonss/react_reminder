49adfca62854634296ff388d52f93b41816be945
Artem <nemezg@gmail.com>
2025-06-30 15:44:30 +0300

    modal

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
+import Modal from "./Modal";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 function PostsLists() {
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      {modalIsOpen && (
+        <Modal onClose={closeModalHandler}>
+          <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+        </Modal>
+      )}
       <ul className={classes.posts}>
-        <Post author="Maxx" body="lalal" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Art" body="not lalal" />
       </ul>
     </>
