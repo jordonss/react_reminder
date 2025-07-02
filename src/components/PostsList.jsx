5bb5d57a1c1b3348f3a16baab2a08cc1b5724733
Artem <nemezg@gmail.com>
2025-07-02 16:56:09 +0300

    fetch

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import Modal from "./Modal";
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -6,10 +6,30 @@ import classes from "./PostsList.module.css";
 
 function PostsLists({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const data = await response.json();
+      setPosts(data.posts);
+      setIsLoading(false);
+    }
+    fetchPosts().catch((error) => {
+      console.error("Failed to fetch posts:", error);
+    });
+  }, []);
 
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((prevPosts) => [postData, ...prevPosts]);
-    onStopPosting();
   }
 
   return (
@@ -19,20 +39,21 @@ function PostsLists({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onCreatePost={addPostHandler} />
         </Modal>
       )}
-      {posts.length === 0 && (
+      {!isLoading && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>No posts yet!</h2>
         </div>
       )}
-      <ul className={classes.posts}>
+      {!isLoading && (<ul className={classes.posts}>
         {posts.map((post, index) => (
-          <Post
-            key={index}
-            author={post.author}
-            body={post.body}
-          />
+          <Post key={index} author={post.author} body={post.body} />
         ))}
-      </ul>
+      </ul>)}
+      {isLoading && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>Loading posts...</h2>
+        </div>
+      )}
     </>
   );
 }
